7c6d9f4d1aca000b04453b3d5cb0a3917cf0cb04
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2025-04-16 14:38:21 +0200

    Added the chase me button and the cringe baby laugh

--- WebDev/first-ts-project/app.ts
+++ WebDev/first-ts-project/app.ts
@@ -2,9 +2,40 @@ const button = document.getElementById('increment') as HTMLButtonElement;
 const countDisplay = document.getElementById('count') as HTMLParagraphElement;
 const resetButton = document.getElementById('reset') as HTMLButtonElement
 const imageContainer = document.getElementById('image-container') as HTMLDivElement
+const audioContainer = document.getElementById('audio-container') as HTMLDivElement
+const chaseButton = document.getElementById('chase') as HTMLButtonElement
 
 let count: number = 0;
 let prank_triggered: boolean = false
+let pos_top: number
+let pos_bottom: number
+let pos_right: number
+let pos_left: number
+
+
+button.addEventListener('click', () => {
+    toggle_prank(true);
+    count++;
+    countDisplay.textContent = "You've clicked " + count.toString() + " times";
+});
+
+resetButton.addEventListener('mouseenter', () => {
+    toggle_prank(false);
+});
+
+resetButton.addEventListener('mouseleave', () => {
+    toggle_prank(true);
+});
+
+chaseButton.addEventListener('mouseover', () => {
+    pos_top = Math.floor(Math.random() * 90);
+    pos_right = Math.floor(Math.random() * 90);
+    chaseButton.style.position = 'absolute';
+    chaseButton.style.top = `${pos_top}%`;
+    chaseButton.style.right = `${pos_right}%`;
+
+    playAudio();
+})
 
 async function getMeme(): Promise<string> {
     const url = "https://api.imgflip.com/get_memes";
@@ -54,16 +85,11 @@ async function toggle_prank(increment: boolean){
         
 }
 
-button.addEventListener('click', () => {
-    toggle_prank(true);
-    count++;
-    countDisplay.textContent = "You've clicked " + count.toString() + " times";
-});
-
-resetButton.addEventListener('mouseenter', () => {
-    toggle_prank(false);
-});
-
-resetButton.addEventListener('mouseleave', () => {
-    toggle_prank(true);
-});
+function playAudio() {
+    audioContainer.innerHTML = '';
+    const audio = document.createElement('audio');
+    audio.src = "tiny-creature-laugh-272424.mp3";
+    audio.play();
+    audioContainer.appendChild(audio);
+    
+}
