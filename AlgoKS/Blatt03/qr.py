51a6c2e99d50da87430e23f5e24065d5aacd7afd
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2025-05-27 23:03:06 +0200

    Also implemented the qr decomposition

--- AlgoKS/Blatt03/qr.py
+++ AlgoKS/Blatt03/qr.py
@@ -15,7 +15,24 @@ def givens_rotation(A, i, j):
     Returns:
         np.ndarray: Rotation Matrix ``J`` as above.
     """
-    pass  # TODO
+    diag_value = A[j,j]
+    elim_value = A[i,j]
+    if diag_value == 0:
+        sign = 1
+    else:
+        sign = np.sign(diag_value)
+    vector_len = np.sqrt(diag_value**2 + elim_value**2)
+
+    c = sign * diag_value / vector_len
+    s = -sign * elim_value / vector_len
+
+    J = np.eye(A.shape[0])
+    J[j, j] = c
+    J[i, i] = c
+    J[j, i] = -s
+    J[i, j] = s
+
+    return J
 
 
 def qr_decompose(A):
@@ -33,7 +50,20 @@ def qr_decompose(A):
     Q = np.eye(m)
     R = A.copy()
 
-    # TODO
+    Js = []
+
+    for i in range(n):
+        for j in range(i+1, m):
+            if R[j,i] == 0:
+                continue
+            J = givens_rotation(R, j, i)
+            R = J @ R
+            Js.insert(0, J)
+
+    for J in Js:
+        Q = Q @ J
+
+    return (Q.T, R)
 
 
 def backward_substitute(R, y):
@@ -46,7 +76,19 @@ def backward_substitute(R, y):
     Returns:
         np.ndarray: Vector ``x``, such that (**) holds.
     """
-    pass  # TODO
+    y_size, x_size = R.shape
+    x_array = [0.] * y_size
+    for j in range(y_size-1 , -1, -1):
+        if j == y_size - 1:
+            x_array[j] =  y[j,0] / R[j,j]
+            continue
+        
+        x_j = y[j, 0]
+        for i in range(y_size - 1, j - 1, -1):
+            x_j -= R[j,i] * x_array[i]
+        x_array[j] = x_j / R[j,j]
+
+    return np.array([x_array]).T
 
 
 def linsolve(A, *bs):
@@ -60,4 +102,11 @@ def linsolve(A, *bs):
     Returns:
         Tuple[np.ndarray, ...]: ``(x_{0}, ..., x_{n - 1})`` as mentioned above.
     """
-    pass  # TODO
+    Q, R = qr_decompose(A)
+    result = []
+
+    for b in bs:
+        new_b = Q.T @ b
+        result.append(backward_substitute(R, new_b))
+
+    return tuple(result)
